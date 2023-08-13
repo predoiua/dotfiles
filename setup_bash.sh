3120e3380a476cd9f3379922c4792c0ee5556d88
AP <predoiu.seli@gmail.com>
2023-08-13 13:54:03 +0300

    fix 2

--- setup_bash.sh
+++ setup_bash.sh
@@ -1,3 +1,8 @@
+to_delete=(.bashrc .bash_profile .bash_logout .bash_login)
+for file in ${to_delete[@]}; do
+    rm -f $HOME/$file
+done
+
 program=bash
 stow -v --target=$HOME $program
 echo "Done $program"
